49457c4e3d924d21364ec72b79a933991c2611e6
jacoboolmedo <64023606+jacoboolmedo@users.noreply.github.com>
2025-02-28 19:22:19 +0100

    4 bien

--- ejercicio4.py
+++ ejercicio4.py
@@ -16,23 +16,23 @@ def qrDetectedCallback():
         robobo.moveWheelsByTime(19.7, 11.5, 4.3)
         robobo.moveWheels(25, 25)
     if qr_data.id == "cruce-izquierda" and distance> 21:
-        # aleatoriamente elige seguir recto o girar a la izquierda
-            decision = random.choice([ "izquierda"])
-            if decision == "recto":
-                print("Decisión: Seguir recto")
-                robobo.moveWheels(20, 20)
-            else:
-                print("Decisión: Girar a la izquierda")
-                robobo.moveWheelsByTime(20, 20, 5.3)  
-                robobo.moveWheelsByTime(19.5, -18,1)  # Gira a la izquierda
-                robobo.moveWheels(20, 20)  # Sigue adelante después de girar
+        # aleatoriamente elige seguir recto o girar a la izquierda (opción comentada)
+            #decision = random.choice([ "recto","izquierda"])
+            #if decision == "recto":
+            #    print("Decisión: Seguir recto")
+            #    robobo.moveWheels(20, 20)
+            #else:
+            #    print("Decisión: Girar a la izquierda")
+            robobo.moveWheelsByTime(20, 20, 5.3)  
+            robobo.moveWheelsByTime(19.5, -18,1)  
+            robobo.moveWheels(20, 20)  
     if qr_data.id == "peatones" and distance > 21.5:
         robobo.moveWheels(0, 0)
         robobo.sayText("No hay peatones, podemos seguir")
         robobo.moveWheels(25, 25)
         robobo.wait(3)
 
-    if qr_data.id == "velocidad10" and distance > 21.5:
+    if qr_data.id == "velocidad 10" and distance > 21.5:
         robobo.sayText("Me pongo a velocidad 10")
         robobo.moveWheels(10, 10)
         robobo.wait(3)
@@ -41,13 +41,11 @@ def qrDetectedCallback():
         robobo.stopMotors()
         robobo.disconnect()
     robobo.wait(0.01)
-# Configurar el callback para detección de QR
+
 robobo.whenAQRCodeIsDetected(qrDetectedCallback)
 
-# Moverse hacia adelante hasta detectar un QR
 robobo.moveWheels(20, 20)
 
-# Mantener el programa en ejecución
 while True:
     robobo.wait(0.1)
 
