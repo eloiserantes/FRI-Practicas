be8a47a70d3f6470df4d1cc9846786d905f81a6e
jacoboolmedo <64023606+jacoboolmedo@users.noreply.github.com>
2025-03-20 11:55:49 +0100

    ej2 done

--- Serantes_Abal_Eloi_Ejercicio2.py
+++ Serantes_Abal_Eloi_Ejercicio2.py
@@ -7,12 +7,13 @@ robobo = Robobo('localhost')
 robobo.connect()
 robobo.moveTiltTo(105, 25)
 robobo.moveWheels(8, -8)
+
 # Parámetros de control
-kp_distance = 0.2  # Ganancia proporcional para la velocidad de aproximación
-kp_posx = 0.55       # Ganancia proporcional para la posición en el eje X
-ki_posx = 0.0005    # Ganancia integral para la posición en el eje X
+kp_distance = 0.07  # Ganancia proporcional para la velocidad de aproximación
+kp_posx = 0.2       # Ganancia proporcional para la posición en el eje X
+ki_posx = 0.001    # Ganancia integral para la posición en el eje X
 kd_posx = 0.02      # Ganancia derivativa para la posición en el eje X
-goal_distance = 184  # Distancia objetivo en mm
+goal_distance = 300  # Distancia objetivo en mm
 goal_posx = 50       # Posición objetivo en el eje X (centro de la cámara)
 
 # Variables para el control PID de la posición en el eje X
@@ -44,12 +45,17 @@ def colordetectcallback():
         correction_posx = error_posx * kp_posx + integral_posx * ki_posx + derivative_posx * kd_posx
         previous_error_posx = error_posx
 
-        # Combinar las correcciones de distancia y posición
-        speed_left = speed_distance + correction_posx
-        speed_right = speed_distance - correction_posx
+        # Si la posición en el eje X está entre 45 y 55, mover en línea recta
+        if 45 <= color_blob.posx <= 55:
+            speed_left = speed_distance
+            speed_right = speed_distance
+        else:
+            # Combinar las correcciones de distancia y posición
+            speed_left = speed_distance + correction_posx
+            speed_right = speed_distance - correction_posx
 
         # Limitar las velocidades para evitar valores extremos
-        max_speed = 25
+        max_speed = 30
         speed_left = max(min(speed_left, max_speed), -5)
         speed_right = max(min(speed_right, max_speed), -5)
 
@@ -65,7 +71,7 @@ def colordetectcallback():
     if ir_distance > goal_distance:
         print("Objeto alcanzado, deteniendo motores.")
         robobo.moveWheels(5,-5)
-        robobo.wait(1)
+        robobo.wait(3)
         robobo.stopMotors()
         robobo.disconnect()
 
